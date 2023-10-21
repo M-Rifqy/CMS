afc270dae9ab7c57c9db87a45f3fee5c31e49bba
Muhammad Rifqy <2ndrifqy@gmail.com>
2023-10-22 00:51:48 +0700

    profile1

--- resources/views/admin/admin_profile.blade.php
+++ resources/views/admin/admin_profile.blade.php
@@ -1,6 +1,170 @@
 @extends('admin.admin_dashboard')
 @section('admin')
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <div class="page-content">
 
+        {{-- <div class="row">
+            <div class="col-12 grid-margin">
+                <div class="card">
+                    <div class="position-relative">
+                        <figure class="overflow-hidden mb-0 d-flex justify-content-center">
+                            <img src="https://via.placeholder.com/1560x370"class="rounded-top" alt="profile cover">
+                        </figure>
+                        <div
+                            class="d-flex justify-content-between align-items-center position-absolute top-90 w-100 px-2 px-md-4 mt-n4">
+                            <div>
+                                <img class="wd-70 rounded-circle" src="https://via.placeholder.com/100x100" alt="profile">
+                                <span class="h4 ms-3 text-dark">Amiah Burton</span>
+                            </div>
+                            <div class="d-none d-md-block">
+                                <button class="btn btn-primary btn-icon-text">
+                                    <i data-feather="edit" class="btn-icon-prepend"></i> Edit profile
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="d-flex justify-content-center p-3 rounded-bottom">
+                        <ul class="d-flex align-items-center m-0 p-0">
+                            <li class="d-flex align-items-center active">
+                                <i class="me-1 icon-md text-primary" data-feather="columns"></i>
+                                <a class="pt-1px d-none d-md-block text-primary" href="#">Timeline</a>
+                            </li>
+                            <li class="ms-3 ps-3 border-start d-flex align-items-center">
+                                <i class="me-1 icon-md" data-feather="user"></i>
+                                <a class="pt-1px d-none d-md-block text-body" href="#">About</a>
+                            </li>
+                            <li class="ms-3 ps-3 border-start d-flex align-items-center">
+                                <i class="me-1 icon-md" data-feather="users"></i>
+                                <a class="pt-1px d-none d-md-block text-body" href="#">Friends <span
+                                        class="text-muted tx-12">3,765</span></a>
+                            </li>
+                            <li class="ms-3 ps-3 border-start d-flex align-items-center">
+                                <i class="me-1 icon-md" data-feather="image"></i>
+                                <a class="pt-1px d-none d-md-block text-body" href="#">Photos</a>
+                            </li>
+                            <li class="ms-3 ps-3 border-start d-flex align-items-center">
+                                <i class="me-1 icon-md" data-feather="video"></i>
+                                <a class="pt-1px d-none d-md-block text-body" href="#">Videos</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div> --}}
+        <div class="row profile-body">
+            <!-- left wrapper start -->
+            <div class="d-none d-md-block col-md-4 col-xl-4 left-wrapper">
+                <div class="card rounded">
+                    <div class="card-body">
+                        <div class="d-flex align-items-center justify-content-between mb-2">
+                            <div>
+                                <img class="wd-100 rounded-circle"
+                                    src="{{ !empty($profileData->photo) ? url('upload/admin_images/' . $profileData->photo) : url('upload/no_image.jpg') }}"
+                                    alt="profile">
+                                <span class="h4 ms-3">{{ $profileData->username }}</span>
+                            </div>
+                        </div>
+                        <div class="mt-3">
+                            <label class="tx-11 fw-bolder mb-0 text-uppercase">Name:</label>
+                            <p class="text-muted">{{ $profileData->name }}</p>
+                        </div>
+                        <div class="mt-3">
+                            <label class="tx-11 fw-bolder mb-0 text-uppercase">Email:</label>
+                            <p class="text-muted">{{ $profileData->email }}</p>
+                        </div>
+                        <div class="mt-3">
+                            <label class="tx-11 fw-bolder mb-0 text-uppercase">Phone:</label>
+                            <p class="text-muted">{{ $profileData->phone }}</p>
+                        </div>
+                        <div class="mt-3">
+                            <label class="tx-11 fw-bolder mb-0 text-uppercase">Address:</label>
+                            <p class="text-muted">{{ $profileData->address }}</p>
+                        </div>
+                        <div class="mt-3 d-flex social-links">
+                            <a href="javascript:;" class="btn btn-icon border btn-xs me-2">
+                                <i data-feather="github"></i>
+                            </a>
+                            <a href="javascript:;" class="btn btn-icon border btn-xs me-2">
+                                <i data-feather="twitter"></i>
+                            </a>
+                            <a href="javascript:;" class="btn btn-icon border btn-xs me-2">
+                                <i data-feather="instagram"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- left wrapper end -->
+            <!-- middle wrapper start -->
+            <div class="col-md-8 col-xl-88 middle-wrapper">
+                <div class="row">
+                    <div class="card">
+                        <div class="card-body">
 
+                            <h6 class="card-title">Update Admin Profile</h6>
 
+                            <form class="forms-sample">
+                                <div class="mb-3">
+                                    <label for="exampleInputUsername1" class="form-label">Username</label>
+                                    <input type="text" class="form-control" id="exampleInputUsername1" name="username"
+                                        autocomplete="off" value="{{ $profileData->username }}">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="exampleInputEmail1" class="form-label">Name</label>
+                                    <input type="text" class="form-control" id="exampleInputUsername1" name="name"
+                                        autocomplete="off" value="{{ $profileData->name }}">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="exampleInputPassword1" class="form-label">Email</label>
+                                    <input type="email" class="form-control" id="exampleInputUsername1" name="email"
+                                        autocomplete="off" value="{{ $profileData->email }}">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="exampleInputPassword1" class="form-label">Phone</label>
+                                    <input type="text" class="form-control" id="exampleInputUsername1" name="phone"
+                                        autocomplete="off" value="{{ $profileData->phone }}">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="exampleInputPassword1" class="form-label">Address</label>
+                                    <input type="text" class="form-control" id="exampleInputUsername1" name="address"
+                                        autocomplete="off" value="{{ $profileData->address }}">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="exampleInputPassword1" class="form-label">Photo</label>
+                                    <input type="file" class="form-control" id="image" name="photo"
+                                        autocomplete="off" value="{{ $profileData->photo }}">
+                                </div>
+                                <div class="mb-3">
+                                    <img class="wd-80 rounded-circle" id="showImage"
+                                        src="{{ !empty($profileData->photo) ? url('upload/admin_images/' . $profileData->photo) : url('upload/no_image.jpg') }}"
+                                        alt="profile">
+                                </div>
+                                <div class="form-check mb-3">
+                                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                    <label class="form-check-label" for="exampleCheck1">
+                                        Remember me
+                                    </label>
+                                </div>
+                                <button type="submit" class="btn btn-primary me-2">Save Changes</button>
+                            </form>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- middle wrapper end -->
+        </div>
+    </div>
+
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('#image').change(function(e) {
+                var reader = new FileReader()
+                reader.onload = function(e) {
+                    $('#showImage').attr('src', e.target.result)
+                }
+                reader.readAsDataURL(e.target.files['0'])
+            })
+        })
+    </script>
 @endsection
