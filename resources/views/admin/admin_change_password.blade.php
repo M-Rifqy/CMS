aec7697ebd12d8c468f882eba2ebf2817517e708
Muhammad Rifqy <2ndrifqy@gmail.com>
2023-10-22 15:47:37 +0700

    edit password1

--- resources/views/admin/admin_change_password.blade.php
+++ resources/views/admin/admin_change_password.blade.php
@@ -101,45 +101,31 @@
                     <div class="card">
                         <div class="card-body">
 
-                            <h6 class="card-title">Update Admin Profile</h6>
+                            <h6 class="card-title">Admin Change Password</h6>
 
-                            <form method="POST" action="{{ route('admin.profile.store') }}" enctype="multipart/form-data"
+                            <form method="POST" action="{{ route('admin.update.password') }}"
                                 class="forms-sample">
                                 @csrf
                                 <div class="mb-3">
-                                    <label for="exampleInputUsername1" class="form-label">Username</label>
-                                    <input type="text" class="form-control" id="exampleInputUsername1" name="username"
-                                        autocomplete="off" value="{{ $profileData->username }}">
+                                    <label for="old_password" class="form-label">Old Password</label>
+                                    <input type="text" class="form-control @error('old_password') is-invalid @enderror"
+                                        id="old_password" name="old_password" autocomplete="off">
+                                    @error('old_password')
+                                        <span class="text-danger">{{ $mesage }}</span>
+                                    @enderror
                                 </div>
                                 <div class="mb-3">
-                                    <label for="exampleInputEmail1" class="form-label">Name</label>
-                                    <input type="text" class="form-control" id="exampleInputUsername1" name="name"
-                                        autocomplete="off" value="{{ $profileData->name }}">
+                                    <label for="new_password" class="form-label">New Password</label>
+                                    <input type="text" class="form-control @error('new_password') is-invalid @enderror"
+                                        id="new_password" name="new_password" autocomplete="off">
+                                    @error('new_password')
+                                        <span class="text-danger">{{ $mesage }}</span>
+                                    @enderror
                                 </div>
                                 <div class="mb-3">
-                                    <label for="exampleInputPassword1" class="form-label">Email</label>
-                                    <input type="email" class="form-control" id="exampleInputUsername1" name="email"
-                                        autocomplete="off" value="{{ $profileData->email }}">
-                                </div>
-                                <div class="mb-3">
-                                    <label for="exampleInputPassword1" class="form-label">Phone</label>
-                                    <input type="text" class="form-control" id="exampleInputUsername1" name="phone"
-                                        autocomplete="off" value="{{ $profileData->phone }}">
-                                </div>
-                                <div class="mb-3">
-                                    <label for="exampleInputPassword1" class="form-label">Address</label>
-                                    <input type="text" class="form-control" id="exampleInputUsername1" name="address"
-                                        autocomplete="off" value="{{ $profileData->address }}">
-                                </div>
-                                <div class="mb-3">
-                                    <label for="exampleInputPassword1" class="form-label">Photo</label>
-                                    <input type="file" class="form-control" id="image" name="photo"
-                                        autocomplete="off" value="{{ $profileData->photo }}">
-                                </div>
-                                <div class="mb-3">
-                                    <img class="wd-80 rounded-circle" id="showImage"
-                                        src="{{ !empty($profileData->photo) ? url('upload/admin_images/' . $profileData->photo) : url('upload/no_image.jpg') }}"
-                                        alt="profile">
+                                    <label for="new_password_confirmation" class="form-label">Confirm New Password</label>
+                                    <input type="text" class="form-control" id="new_password_confirmation"
+                                        name="new_password_confirmation" autocomplete="off">
                                 </div>
                                 <button type="submit" class="btn btn-primary me-2">Save Changes</button>
                             </form>
@@ -150,16 +136,4 @@
             <!-- middle wrapper end -->
         </div>
     </div>
-
-    <script type="text/javascript">
-        $(document).ready(function() {
-            $('#image').change(function(e) {
-                var reader = new FileReader()
-                reader.onload = function(e) {
-                    $('#showImage').attr('src', e.target.result)
-                }
-                reader.readAsDataURL(e.target.files['0'])
-            })
-        })
-    </script>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,7 @@ Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
     Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
     Route::post('/admin/profile/store', [AdminController::class, 'AdminProfileStore'])->name('admin.profile.store');
+    Route::get('/admin/change/password', [AdminController::class, 'AdminChangePassword'])->name('admin.change.password');
     // Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
 });
 
